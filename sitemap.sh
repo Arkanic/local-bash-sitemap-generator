4527f507fac0034b4215839df78163b8e1d617c8
Arkanic <50847107+Arkanic@users.noreply.github.com>
2020-10-28 09:28:33 +1300

    custom url

--- sitemap.sh
+++ sitemap.sh
@@ -1,6 +1,7 @@
+url="www.example.com"
 find . -type f -not -path "./.git/*" > sitemap-temp.txt
 stdbuf -o0 cut -c2- sitemap-temp.txt > sitemap-temp2.txt
-for i in $(cat sitemap-temp2.txt); do echo "https://arkanic.github.io$i"; done > sitemap-temp3.txt
+for i in $(cat sitemap-temp2.txt); do echo "$url$i"; done > sitemap-temp3.txt
 for i in $(cat sitemap-temp3.txt); do echo $i | awk '{gsub("index.html",""); print}'; done > sitemap.txt
 sed -i "/sitemap-temp.txt/d" sitemap.txt
 sed -i "/sitemap-temp2.txt/d" sitemap.txt
